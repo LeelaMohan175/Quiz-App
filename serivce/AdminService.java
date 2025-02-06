7c5e7bdeeaafaa4a4686ff651665efca48fa9a6e
Leela Mohan <leelamohanbr175@gmail.com>
2025-02-06 22:35:46 +0530

    Quiz App Admin functionality

---
+++ serivce/AdminService.java
@@ -0,0 +1,36 @@
+package serivce;
+
+import model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AdminService {
+
+
+
+    //Add qsn
+    public void addQsn(Question qsn, ArrayList<Question> list){
+        list.add(qsn);
+    }
+
+    //Delete qsn -- for now index will be qsnId
+    public void removeQsn(int qsnId,ArrayList<Question> list){
+        list.remove(qsnId);
+    }
+
+    //Edit qsn
+    public void editQsn(ArrayList<Question> list, int qsnId, String newQsn){
+        Question qsn = list.get(qsnId);
+        qsn.setQuestion(newQsn);
+    }
+
+    //DIsplay questions
+
+    public void display(ArrayList<Question> list){
+       for (Question qsn: list){
+           System.out.println(qsn.toString());
+       }
+    }
+
+}
